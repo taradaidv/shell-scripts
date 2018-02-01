04ec9d6714bb36686daaab2eb586367e04b528fd
Dmitrii Taradai <35196630+taradaidv@users.noreply.github.com>
2018-02-01 14:05:27 +0300

    Update dns.sh
    Use:
     ./dns.sh in.txt

--- sh/dns.sh
+++ sh/dns.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while IFS='' read -r line; do
-    host -t txt $line >> out.txt.txt
+#    host -t txt $line >> out.txt.txt
     host -t mx $line >> out.mx.txt
-done < "in.txt"
+done < "$1"

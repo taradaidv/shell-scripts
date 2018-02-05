c848dc596284ee3849be5277350aada350bb0a7c
Dmitrii Taradai <35196630+taradaidv@users.noreply.github.com>
2018-02-05 09:29:43 +0300

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -3,6 +3,6 @@ while IFS='' read -r line; do
 #host -t txt  _dmarc.$line  >> out.DMARC.txt
 #host -t txt default._domainkey.$line  >> out.DKIM.txt
 #host -t txt mail._domainkey.$line  >> out.DKIM.txt
-#    host -t txt $line >> out.TXT.txt
-#    host -t mx $line >> out.MX.txt
+#host -t txt $line >> out.TXT.txt
+#host -t txt mx $line >> out.MX.txt
 done < "$1"

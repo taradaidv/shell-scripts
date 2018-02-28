64a9b5cd20f78e8bc92d82cf6b06c9d6bd930c32
user <user@system.local>
2018-02-28 23:25:26 +0300

    on
    only numbers

---
+++ brute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+i="0"
+clear
+while [ a=a ]
+do		
+	
+	h=$(md5 -s "$i")
+
+  if [[ $h = 'MD5 ("'$i'") = f899139df5e1059396431415e770c6dd' ]] #EXAMPLE MD5 -s 100 = f899139df5e1059396431415e770c6dd
+    then 
+    break
+  else
+  i=$[$i+1]
+  fi
+done
+echo "[FIND] $h"
+#echo "[FIND] $h" > ~/FIND.TXT4

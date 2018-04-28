ad36c610a03c374f7af2f64f070e5e9de2473c4e
Dmitrii Taradai <35196630+taradaidv@users.noreply.github.com>
2018-04-28 09:56:22 +0300

    Update cms_scan.sh

--- sh/cms_scan.sh
+++ sh/cms_scan.sh
@@ -1,39 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 clear
 echo "Find word in HTML v.1.5 [28.09.2018]"
 if [ -z "$1" ] & [ -z "$2" ] & [ -z "$3" ];then
- echo "Введите в качестве дополнительных параметров разделенных пробелом:\n[количество хостов] [IP-адрес без последнего октета] [слово для поиска (регистр учитывается)]\nНапример: $0 254 192.168.0 Drupal"
+ echo "staroe opisanie !!!Введите в качестве дополнительных параметров разделенных пробелом:\n[количество хостов] [IP-адрес без последнего октета] [слово для поиска (регистр учитывается)]\nНапример: $0 254 192.168.0 Drupal"
  exit 1
 fi
 
 
 #net loop
-z=0
-while (( ++z <= $1 ))
+y=$1
+while (( ++y <= $2 ))
 do
-     ip="$2.$z"
+# x.x.[x].x
+z=$3
+while (( ++z <= $4 ))
+do
+# x.x.x.[x]
+     ip="$5.$y.$z"
 #net scan
-echo "Scan: "$ip
-     for port in {80,8080}; do
-		content=$(curl --insecure -s -m2 -L 'http://'$ip':'$port)
-		(timeout 1 bash -c "</dev/tcp/$ip/$port") &>/dev/null && ([[ "$content" == *$3* ]] && echo ">>>[$3] = http://$ip:$port")
-	done
-	
-	for port in {443,8433}; do
-		content=$(curl --insecure -s -m2 -L 'https://'$ip':'$port)
-		(timeout 1 bash -c "</dev/tcp/$ip/$port") &>/dev/null && ([[ "$content" == *$3* ]] && echo ">>>[$3] = https://$ip:$port")
-		
-	done
+echo "Scan: $ip"
+        for port in {80,8080}; do
+                #echo "$port"
+                content=$(curl --insecure -s -m2 -L 'http://'$ip':'$port)
+                [[ "$content" == *$6* ]] && echo ">>>[$6] = http://$ip:$port"
+        done
 
-#	
-#pstr="[====================================================================]"
-#i=0
-#while [ $i -lt $1 ]; do
-#  sleep 0.1 # this is work
-#  i=$(( $i + 1 ))
-#  pd=$(( $i * 70 / $1 ))
-#  printf "\r%3d.%1d%% %.${pd}s" $(( $i * 100 / $1 )) $(( ($i * 1000 / $1) % 10 )) $pstr
-#done
-#echo "\n"
+         for port in {443,8443}; do
+                echo "$port"
+                content=$(curl --insecure -s -m2 -L 'https://'$ip':'$port)
+                [[ "$content" == *$6* ]] && echo ">>>[$6] = https://$ip:$port"
+        done
 
+done
 done

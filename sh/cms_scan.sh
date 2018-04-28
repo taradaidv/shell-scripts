4ea74a746e900f8a05e9c137fdc791ed55c89657
Dmitrii Taradai <35196630+taradaidv@users.noreply.github.com>
2018-04-28 13:11:03 +0300

    Update cms_scan.sh

--- sh/cms_scan.sh
+++ sh/cms_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "Find word in HTML v.1.6 [28.09.2018]"
+echo "Find word in HTML v.1.5 [28.09.2018]"
 if [ -z "$1" ] & [ -z "$2" ] & [ -z "$3" ];then
  echo "staroe opisanie !!!Введите в качестве дополнительных параметров разделенных пробелом:\n[количество хостов] [IP-адрес без последнего октета] [слово для поиска (регистр учитывается)]\nНапример: $0 254 192.168.0 Drupal"
  echo "./cms_scan.sh 192.168 128 143 1 254 Drupal"
@@ -21,15 +21,15 @@ do
 #net scan
 echo "Scan: $ip"
         for port in {80,8080}; do
-                echo "$port"
-                #content=$(curl --insecure -s -m2 -L 'http://'$ip':'$port)
-                #[[ "$content" == *$6* ]] && echo ">>>[$6] = http://$ip:$port"
+                #echo "$port"
+                content=$(curl --insecure -s -m1 -L 'http://'$ip':'$port)
+                [[ "$content" == *$6* ]] && echo ">>>[$6] = http://$ip:$port"
         done
 
          for port in {443,8443}; do
-                echo "$port"
-                #content=$(curl --insecure -s -m2 -L 'https://'$ip':'$port)
-                #[[ "$content" == *$6* ]] && echo ">>>[$6] = https://$ip:$port"
+                #echo "$port"
+                content=$(curl --insecure -s -m1 -L 'https://'$ip':'$port)
+                [[ "$content" == *$6* ]] && echo ">>>[$6] = https://$ip:$port"
         done
 
 done
